caba24fa3b77afc5041abe09340d131df14811c7
szchmausser <szchmausser@gmail.com>
2024-08-22 10:59:20 -0400

    Instalado Flowbite: https://flowbite-react.com/docs/getting-started/introduction
    npm install flowbite-react
    
    * tailwind.config.js
    
      import flowbite from "flowbite-react/tailwind";
    
      /** @type {import('tailwindcss').Config} */
      export default {
        content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
        theme: {
          extend: {},
        },
        plugins: [flowbite.plugin()],
      };

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "flowbite-react";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </>
   );
 }
